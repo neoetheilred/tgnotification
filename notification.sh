7813fdc0a5b120bd635450b71ec10e57a26f7762
roman <rdandrievskiy@edu.hse.ru>
2023-06-25 12:59:13 +0200

    fix

--- notification.sh
+++ notification.sh
@@ -1,8 +1,6 @@
 #!/bin/sh -l
 
 echo "{\"chat_id\"=\"$2\", \"text\":\"$3\"}\n"
-response=$(curl -d "{\"chat_id\"=\"$2\", \"text\":\"$3\"}" \
-    https://api.telegram.org/bot$1/sendMessage \
-    -H 'Content-Type: application/json')
+response=$(curl -d "{\"chat_id\"=\"$2\", \"text\":\"$3\"}" https://api.telegram.org/bot$1/sendMessage -H 'Content-Type: application/json')
 echo $response
 echo "response=$response" >> $GITHUB_OUTPUT
