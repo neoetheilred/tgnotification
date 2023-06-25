b31352b3780d646c77b1b9af3299d552f0bdfc1e
roman <rdandrievskiy@edu.hse.ru>
2023-06-25 16:17:36 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,11 @@
 #!/bin/sh -l
 
-body="{\"chat_id\"=\"$2\", \"text\":\"$3\"}"
+body="{\"chat_id\": \"$2\", \"text\":\"$3\"}"
 
 echo $body
-# curl -X POST \
-#      -H 'Content-Type: application/json'\
-#      -d "{\"chat_id\": \"$2\", \"text\": \"$3\"}" \
-#      https://api.telegram.org/bot$1/sendMessage
 response=$(curl -X POST \
     -H 'Content-Type: application/json' \
-    -d "{\"chat_id\": \"$2\", \"text\":\"$3\"}" \
+    -d $body \
     https://api.telegram.org/bot$1/sendMessage)
 # response=$(curl https://api.telegram.org/bot$1/getMe)
 echo $response
